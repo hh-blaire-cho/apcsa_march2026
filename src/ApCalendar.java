7051d84c499df70860360a003ede8a1f70b7f182
Blaire <hh.blaire.cho@gmail.com>
2026-03-29 12:48:56 +0900

    part labeling

--- src/ApCalendar.java
+++ src/ApCalendar.java
@@ -20,11 +20,13 @@ public class ApCalendar {
   }
 
   public static void main(String[] args) {
+    // part a
     System.out.println(ApCalendar.isLeapYear(2020));
     System.out.println(ApCalendar.isLeapYear(2026));
     System.out.println(ApCalendar.isLeapYear(2028));
     System.out.println(ApCalendar.isLeapYear(2100));
 
+    // part b
     System.out.println(ApCalendar.numberOfLeapYears(2020, 2028)); // should be 3. (2020, 2024, 2028)
     System.out.println(ApCalendar.numberOfLeapYears(2099, 2107)); // should be 1. 2100 does not count. Only 2104 count.
 
